3a1fae78e1ebafc3209fea29e30e544e825e0ef7
13lackninja <jonathan.crawford55@gmail.com>
2018-07-09 13:06:39 -0700

    local161 site basic updates

--- publicsf.js
+++ publicsf.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const app = express();
 const pages = require('./routes/pages');
+const bs = require('browser-sync').create();
 
 // Middlewear
 
@@ -12,3 +13,19 @@ app.use('/publicsf', pages);
 // Port
 
 app.listen(8090);
+
+// Browser sync
+
+const bsOptions = {
+  proxy: 'localhost:8090/publicsf',
+  files: [
+    './public/markup/*.html',
+    './public/styles/*.css',
+    './public/scrips/*.js',
+    '.public/images/*.jpg'
+  ]
+};
+
+bs.init(bsOptions);
+
+bs.reload('*.html', '*.css', '*.js');
