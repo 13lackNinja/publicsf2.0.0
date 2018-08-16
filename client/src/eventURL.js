24fe5fd26170a7dba6f53390450f23e2a75977cc
13lackninja <jonathan.crawford55@gmail.com>
2018-08-15 18:04:33 -0700

    query string stuff

--- client/src/eventURL.js
+++ client/src/eventURL.js
@@ -1,6 +1,6 @@
+
 const eventURL = {
-  base: `https://www.eventbriteapi.com/v3/organizers/8182660460/events/`,
-  token: `/?token=${process.env.REACT_APP_EVENTBRITE_API_KEY}`
+  base: 'https://www.eventbriteapi.com/v3/events/search/?organizer.id=8182660460&include_unavailable_events=false&sort_by=date'
 }
 
 export default eventURL
