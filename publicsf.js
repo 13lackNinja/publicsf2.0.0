ebf9ee418e82feb67125002e4043fa989b94607e
13lackninja <jonathan.crawford55@gmail.com>
2018-09-06 14:03:29 -0700

    contact

--- publicsf.js
+++ publicsf.js
@@ -5,7 +5,7 @@ const app = express();
 
 const contactRouter = require('./routes/contact');
 
-app.use('/api/contact', contactRouter);
+app.post('/api/contact', contactRouter);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
