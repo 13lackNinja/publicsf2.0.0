e65b6e92d77fbb7ba598ac6b00c7e94b97d7666e
13lackninja <jonathan.crawford55@gmail.com>
2018-09-18 14:21:17 -0700

    ticketing

--- client/src/eventRequest.js
+++ client/src/eventRequest.js
@@ -1,11 +1,11 @@
 
 const eventRequest = {
-  base: 'https://www.eventbriteapi.com/v3/events/search/?organizer.id=17405142071&include_unavailable_events=false&sort_by=date',
+  base: 'https://www.eventbriteapi.com/v3/events/search/?organizer.id=17405142071&include_unavailable_events=true&sort_by=date',
   headers: {
     headers: {
-      'Authorization': `Bearer ${process.env.REACT_APP_EVENTBRITE_API_KEY}`
+      'Authorization': `Bearer BWOB5MWVVQUGFDHOKMY4`
     }
-    }
+  }
 }
 
 export default eventRequest
